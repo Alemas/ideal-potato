b561ae2fc5829a81386d31cd6de5549a380c4f53
Mateus <mateusreckziegel@gmail.com>
2017-04-18 22:02:59 -0300

    Create CreateTable.sql

---
+++ CreateTable.sql
@@ -0,0 +1,10 @@
+drop table product;
+
+create table product ( id int,
+					Title varchar(255),
+					Author varchar(255) not null,
+					Material varchar(100) not null,
+					Price decimal(5,2) not null,
+					Release_Date date not null,
+					primary key (id)
+					);
